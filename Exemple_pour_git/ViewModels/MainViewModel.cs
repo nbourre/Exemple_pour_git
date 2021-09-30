9a5393942bc5464668d0fe49361d1fbabf2ec953
nbourre <nbourrereg@hotmail.com>
2021-09-30 09:00:22 -0400

    Add project files.

---
+++ Exemple_pour_git/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Exemple_pour_git.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ Exemple_pour_git/Views/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Exemple_pour_git.ViewModels;
+using System.Windows;
+
+namespace Exemple_pour_git
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+    }
+}
